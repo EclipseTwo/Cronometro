33377cc292dd36b78d1648389e5c049285bb07fa
yOriginTwo <orlsgabriel09@gmail.com>
2025-01-03 22:58:38 -0300

    Cronometro / Pausar Funcionando

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
-const tempoAtual = Date.now();
+let tempoAtual = 0;
 let IntervalId;
+let tempoPausado = 0;
 
 function Iniciar() {
+    if (!tempoAtual) {
+        tempoAtual = Date.now();
+    } else {
+        tempoAtual = Date.now() - tempoPausado;
+    }
     IntervalId = setInterval(Rodando, 1000);
 }
 
@@ -9,24 +15,35 @@ function Rodando() {
     const cronometro = Date.now() - tempoAtual;
 
     let seg = Math.floor(cronometro / 1000);
-
     let min = 0;
     let hora = 0;
     let dia = 0;
     const containerDia = document.getElementById('container2');
 
-    if (seg == 60) { min++; seg = 0; }
-    else if (min == 60) { hora++; min = 0; }
-    else if (hora == 24) { dia++; hora = 0; containerDia.style.opacity = "1"; containerDia.style.visibility = "visible"; }
+    if (seg >= 60) {
+        min = Math.floor(seg / 60);
+        seg = seg % 60;
+    }
+    if (min >= 60) {
+        hora = Math.floor(min / 60);
+        min = min % 60;
+    }
+    if (hora >= 24) {
+        dia = Math.floor(hora / 24);
+        hora = hora % 24;
+        containerDia.style.opacity = "1";
+        containerDia.style.visibility = "visible";
+    }
 
     seg = seg < 10 ? "0" + seg : seg;
     min = min < 10 ? "0" + min : min;
     hora = hora < 10 ? "0" + hora : hora;
 
-    var tempo = hora + ":" + min + ":" + seg;
+    const tempo = hora + ":" + min + ":" + seg;
     document.getElementById('crono').innerHTML = tempo;
-};
+}
 
 function Parar() {
     clearInterval(IntervalId);
+    tempoPausado = Date.now() - tempoAtual;
 }
